49c7b4c4852afd3d0a02f2e5ea214112607bc7f6
Cudilala <augustinemadu9@gmail.com>
2023-06-30 20:25:00 +0100

    :construction: fixing og:image issues

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,6 +18,9 @@
 	<meta property="og:type" content="website" />
 	<meta property="og:locale" content="en_GB" />
 	<meta property="og:image" content="https://cudi.dev/og-image.png" />
+	<meta property="og:image:width" content="1000" />
+	<meta property="og:image:height" content="750" />
+
 	<meta name="twitter:card" content="summary" />
 	<meta name="twitter:site" content="@CudiLala_" />
 	<meta name="twitter:creator" content="@CudiLala_" />
