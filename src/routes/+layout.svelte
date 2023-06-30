4db0ef725f5710a86e3b68ccb3c7531f239a487b
Cudilala <augustinemadu9@gmail.com>
2023-06-30 20:34:37 +0100

    Revert ":construction: fixing og:image issues"
    This reverts commit 8e8cce46ba6a54780daa82f0edf862b031acd342.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -20,6 +20,17 @@
 	<meta property="og:image" content="https://cudi.dev/og-image.png" />
 	<meta property="og:image:width" content="1000" />
 	<meta property="og:image:height" content="750" />
+
+	<meta name="twitter:card" content="summary" />
+	<meta name="twitter:site" content="@CudiLala_" />
+	<meta name="twitter:creator" content="@CudiLala_" />
+	<meta name="twitter:title" content="High Performace Web Application Developer" />
+	<meta
+		name="twitter:description"
+		content="Hi. I am Augustine Madu, mostly known as Cudi. I am a software developer and I build high performance web applications. If you are in need of a web developer for your business application, or just for questions and collaborations, You can email me at partner@cudi.dev"
+	/>
+	<meta property="twitter:url" content="https://cudi.dev" />
+	<meta property="twitter:image" content="https://cudi.dev/og-image.png" />
 </svelte:head>
 
 <Header />
