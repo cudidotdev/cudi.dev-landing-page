4234d73b8e868bb00e990690a1d1d45cf37eece2
Cudilala <augustinemadu9@gmail.com>
2023-06-30 21:06:32 +0100

    :construction: mobile font test

--- src/routes/hero.svelte
+++ src/routes/hero.svelte
@@ -1,7 +1,7 @@
 <div class="border-b-8 border-b-custom-600">
 	<div class="px-5 sm:px-10 pb-8 smc:pb-10 max-w-screen-xl mx-auto grid grid-cols-1 lg:grid-cols-2">
 		<div
-			class="flex flex-col text-base pt-4 pb-8 sm:pb-10 sm:text-lg gap-6 max-w-screen-sm lg:bg-none bg-watch-0.3 bg-contain bg-no-repeat bg-center"
+			class="flex flex-col text-base py-8 sm:pb-10 sm:text-lg gap-8 max-w-screen-sm lg:bg-none bg-watch-0.3 bg-contain bg-no-repeat bg-center"
 		>
 			<h1>
 				Hi. I am Augustine, mostly known as Cudi. I am a software developer and I build high
